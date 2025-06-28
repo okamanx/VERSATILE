25e01a899568b7a902bf50b251573f6be256fb27
SENPAI SPIDER <155311536+okamanx@users.noreply.github.com>
2025-06-28 21:10:24 +0530

    Delete fastapi-app directory

--- fastapi-app/constants.py
+++
@@ -1,8 +0,0 @@
-USER_TYPE_PLAYER = "player"
-USER_TYPE_ORG = "org"
-USER_TYPE_ADMIN = "admin"
-
-GIG_STATUS_OPEN = "open"
-GIG_STATUS_CLOSED = "closed"
-
-REPUTATION_MESSAGE = "Endorsed SkillLink Talent 💎"
